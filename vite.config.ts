bce28c8f7afd4f29b6b95892fe932a522068cd14
Spinacia <duskshadow@foxmail.com>
2023-09-28 13:56:21 +0800

    feat: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
+const pathSrc = path.resolve(__dirname, 'src')
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    alias: {
+      '@': pathSrc
+    }
+  }
 })
