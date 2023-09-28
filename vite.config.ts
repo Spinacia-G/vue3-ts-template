49d14ed9075302e819d505649a7eef1f49854aa7
Spinacia <duskshadow@foxmail.com>
2023-09-28 17:07:24 +0800

    feat: element-plus and auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import eslintPlugin from 'vite-plugin-eslint'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 const pathSrc = path.resolve(__dirname, 'src')
 export default defineConfig({
@@ -11,6 +14,15 @@ export default defineConfig({
       include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
       cache: false,
       fix: true
+    }),
+    AutoImport({
+      imports: ['vue'],
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts')
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, 'components.d.ts')
     })
   ],
   resolve: {
