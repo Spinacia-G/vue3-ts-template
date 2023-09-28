a06dcfc963acf04b46a733ae2850fa595bc0a86a
Spinacia <duskshadow@foxmail.com>
2023-09-28 14:30:16 +0800

    feat: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import eslintPlugin from 'vite-plugin-eslint'
 
 const pathSrc = path.resolve(__dirname, 'src')
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: [
+        'src/**/*.ts',
+        'src/**/*.vue',
+        'src/*.ts',
+        'src/*.vue'
+      ],
+      cache: false,
+      fix: true
+    })
+  ],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
     alias: {
